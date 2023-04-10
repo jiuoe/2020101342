08c97cf0aadff267e3a135f9d616c2885d1ad277
jiuoe <u303980269@163.com>
2023-04-10 23:09:35 +0800

    first

---
+++ app/src/main/java/com/example/cnedujnu2020101342/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.cnedujnu2020101342;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
