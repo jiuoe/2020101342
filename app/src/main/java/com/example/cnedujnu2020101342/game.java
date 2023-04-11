2453fd537bc9dd4cbcf48318065093db28029472
jiuoe <u303980269@163.com>
2023-04-11 17:17:59 +0800

    test four finish

--- app/src/main/java/com/example/cnedujnu2020101342/game.java
+++ app/src/main/java/com/example/cnedujnu2020101342/game.java
@@ -1,7 +1,7 @@
 package com.example.cnedujnu2020101342;
 
 public class game {
-    private final int[] rolls = new int[21];
+    private final int[] rolls = new int[22];
     private int currentRoll = 0;
 
     public void roll(int i) {
@@ -16,15 +16,23 @@ public class game {
             {
                 score += 10 + rolls[frameIndex + 2];
                 frameIndex += 2;
-            } else {
+            }
+            if(isSTRIKE(frameIndex)){
+                score += 10 +rolls[frameIndex+1]+rolls[frameIndex + 2];
+                frameIndex += 1;
+            }else {
                 score += rolls[frameIndex] + rolls[frameIndex + 1];
                 frameIndex += 2;
             }
+
 
         }
         return score;
     }
 
+    private boolean isSTRIKE(int frameIndex){
+        return rolls[frameIndex]==10;
+    }
     private boolean isSpare(int frameIndex) {
         return rolls[frameIndex] +
                 rolls[frameIndex + 1] == 10;
